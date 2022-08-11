05b81cfc31e96b777f9efdfb6bdd80616c238893
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-08-11 18:34:11 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,39 +1,38 @@
-BtnStartEl = document.querySelector('button[data-start]');
-BtnStopEl = document.querySelector('button[data-stop]');
-const ACTION_DELAY = 1000;
-const onInretval = {
-    timerId: null,
-    isActiveBtn: false,
-}
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let timerId = null;
+let firstRun = true;
 
-BtnStartEl.addEventListener("click", onStart);
-BtnStopEl.addEventListener("click", onStop);
+buttonStop.disabled = true;
 
+buttonStart.addEventListener('click', runColor);
+buttonStop.addEventListener('click', stopColor);
+
+function runColor(){
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(colorChange, 1000);
+    //1 зміна кольору одразу після натискання кнопки
+    if (firstRun){
+        colorChange();
+        firstRun = false;
+    };
+};
+
+function stopColor(){
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+    firstRun = true;
+};
+
+//змінює колір фону для <body>
+function colorChange(){
+    bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+//генерує випадковий колір
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function onStart() {  
-    if (onInretval.isActiveBtn) {
-        return;
-    }
-        onInretval.isActiveBtn = true;
-        BtnStartEl.setAttribute('disabled', true);
-        BtnStopEl.removeAttribute('disabled');
-            onInretval.timerId = setInterval(() => {
-            document.body.style.backgroundColor = getRandomHexColor();
-            }, ACTION_DELAY);
-    console.log('onStart');
-}
-
-function onStop() {
-    if (BtnStartEl.hasAttribute('disabled')) {
-        BtnStopEl.setAttribute('disabled', true);
-        BtnStartEl.removeAttribute('disabled');
-        clearInterval(onInretval.timerId);
-        onInretval.isActiveBtn = false;
-    } else {
-        return;
-    }
-    console.log('onStop');
-}
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
